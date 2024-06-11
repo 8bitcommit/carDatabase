84b47899edf6b3618b5a67cf8e2ba4aee58ece11
Adam Kearsey <104175971+8bitcommit@users.noreply.github.com>
2024-06-11 16:33:42 -0600

    query attempts
    sql queries for reports page.

--- query.sql
+++ query.sql
@@ -6,7 +6,18 @@ SELECT * from VehicleType;
 SELECT * from Customer;
 SELECT * from Employee;
 SELECT * from Branch;
+--1
+SELECT vehicle.VType from Rental, Vehicle
+where DateRented like '%-03-%' and rental.VIN=Vehicle.VIN
+group by vehicle.VType
+having count(*)>=0;
 
-SELECT * from Rental
-where DateRented > '2024-05-20';
+--2
 
+select  t1.Make,t1.Model,max(num_rentals) as num
+from (SELECT distinct rental.ReturnedTo ,count(rental.ReturnedTo) as num_rentals,vehicle.Make,Vehicle.Model from Rental, Vehicle
+where rental.VIN=Vehicle.VIN and Vehicle.Kilometers<20000
+group by rental.ReturnedTo,vehicle.Make,Vehicle.Model)as t1
+group by t1.Make,t1.Model;
+
+--3
